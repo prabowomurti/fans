b4942d8923da9a3e1f7fc2b82b552df2efe6f50d
Prabowo Murti <prabowo.murti@gmail.com>
2012-01-07 01:53:54 +0700

    Initial files

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Fans Prabowo Murti</title>
+		<link href="assets/css/style-common.css" rel="stylesheet" />
+		<meta http-equiv="Content-type" content="text/html;charset=UTF-8">
+		<meta name="author" content="Prabowo Murti" >
+		<meta name="description" content="Prabowo Murti Fans Club" >
+		<meta name="keywords" content="Prabowo Murti, fans club, minimalist, simple theme" >
+		<script type="text/javascript" src="assets/js/jquery-1.7.1.min.js"></script>
+		<script type="text/javascript" src="assets/js/script-functions.js"></script>
+		<script type="text/javascript" src="assets/js/script-common.js"></script>
+	</head>
+	<body>
+		<div class="container" >
+			<div class="loading_message">relax</div>
+			<div class="content">
+				<div class="row_1">
+					<div class="col_1"></div>
+					<div class="col_2"></div>
+					<div class="col_3"></div>
+				</div>
+				<div class="row_2">
+					<div class="col_1"></div>
+					<div class="col_2" id="the_box">I am the centre of universe</div>
+					<div class="col_3"></div>
+				</div>
+				<div class="row_3">
+					<div class="col_1"></div>
+					<div class="col_2"></div>
+					<div class="col_3"></div>
+				</div>
+				
+			</div>
+		</div>
+	</body>
+</html>
